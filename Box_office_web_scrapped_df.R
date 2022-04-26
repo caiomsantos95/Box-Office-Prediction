4dc92d8bb46e77ddb523ce5b7b60fdf8264bac06
Caio Marques <caiodossantos@Caios-MacBook-Pro.local>
2022-04-25 23:12:31 -0400

    Adjusted for factorizationa and new data

--- Box_office_web_scrapped_df.R
+++ Box_office_web_scrapped_df.R
@@ -22,7 +22,9 @@ head(web_scrapping)
 skim(movies)
 summary(web_scrapping)
 summary(movies)
-web_scrapping <- readr::read_csv("movies_2017.csv")
+df_2016_17 <- readr::read_csv("movies_2016_2017.csv")
+df_2015 <- readr::read_csv("movies_2015.csv")
+web_scrapping <- union(df_2016_17, df_2015)
 view(web_scrapping)
 
 ### DEBUGGING
@@ -39,13 +41,12 @@ df$released <- gsub(",","", df$released)
 df$released <- gsub("(United States)","", df$released)
 df$released <- substr(df$released,1,nchar(df$released)-2)
 df$released <- as.Date(df$released, "%B %d %Y")
-summary(df)
 
 ###ADJUSTING IDs
-df$URL_new <- str_match(df$movie_url, "title/\\s*(.*?)\\s*/?ref")
-df$URL_new <- str_extract_all(df$movie_url,"(?<=title/).+(?=/?ref)")
-df$id <- substr(df$URL_new,1,nchar(df$URL_new)-2)
-view(df)
+#df$URL_new <- str_match(df$movie_url, "title/\\s*(.*?)\\s*/?ref")
+#df$URL_new <- str_extract_all(df$movie_url,"(?<=title/).+(?=/?ref)")
+#df$id <- substr(df$URL_new,1,nchar(df$URL_new)-2)
+#view(df)
 
 #### DROPPING COLUMNS
 ### FILTERING OUT EVERY MOVIE WITHOUT BUDGET INFORMATION 
@@ -54,6 +55,8 @@ df <- df %>% filter(is.na(budget) == FALSE)
 ### FILTERING OUT EVERY MOVIE WITHOUT BOX OFFICE INFORMATION 
 df <- df %>% filter(is.na(`gross value`) == FALSE)
 
+### FILTERING TV MOVIES LAUNCHED MOVIE THEATER
+df <- df %>% filter(rating == "PG-13" | rating == "R" | rating == "Not Rated" | rating == "PG" | is.na(rating) == TRUE | rating =="G" | rating == "Unrated" | rating == "NC-17")
 
 ### REPLACING THE CURRENCY SIGNES WITH TEXT 
 unique(df$`budget currency`)
@@ -69,7 +72,7 @@ df$`budget currency` <-  ifelse(df$`budget currency` == "$", "USD",
                          ifelse(df$`budget currency` == "MX$","MXN",
                          ifelse(df$`budget currency` == "ISK","ISK",
                          ifelse(df$`budget currency` == "CA$","CAD",
-                         ifelse(df$`budget currency` == "RUR","RUB", "BRL"
+                         ifelse(df$`budget currency` == "RUR","RUB", "Other"
                          ))))))))))
 df$`gross currency` <- ifelse(df$`gross currency` == "$", "USD", "BRL")
 
@@ -77,10 +80,8 @@ df$`gross currency` <- ifelse(df$`gross currency` == "$", "USD", "BRL")
 df$year_widely_released <- year(df$released)
 df$week_widely_released <- week(df$released)
 
-view(df)
-
 ##ENRICHING DATA TABLE
-df <- merge(x = df, y = movies, by.x = "id", by.y = "tconst", all.x=TRUE)
+df <- merge(x = df, y = movies, by.x = "movie_id", by.y = "tconst", all.x=TRUE)
 
 ### ADJUSTING GENRES
 library(tm)
@@ -101,42 +102,24 @@ df$runtimeMinutes <- as.numeric(df$runtimeMinutes)
 ### CREATE CURRENCY EXCHANGE RATE TABLE TO UPDATE VALUES FROM INTERNATIONAL MOVIES - REPLICATE FOR ALL CURRENCIES
 install.packages("priceR")
 library(priceR)
-USD_GBP <- historical_exchange_rates("USD", to = "GBP", start_date = "2010-01-01", end_date = "2021-12-31")
+USD_GBP <- historical_exchange_rates("USD", to = "GBP", start_date = "2010-01-01", end_date = "2010-12-31")
 USD_GBP$year <- year(USD_GBP$date)
 table(USD_GBP$year, mean(USD_GBP$one_USD_equivalent_to_x_GBP))
 
 summary <- summarise(USD_GBP, group_by(USD_GBP$year), avg_USD_GBP = mean(USD_GBP$one_USD_equivalent_to_x_GBP))
-summary
-
 
 ### TRAINING THE MODEL WITH USD BUDGET ONLY UNTIL CONVERSION IS OK
 df_usd <- df %>% filter(df$`budget currency` == "USD")
 view(df_usd)
 
+### ORDERING RATING
+unique(df$rating)
+df$rating <- ifelse(df$rating == "Not Rated" | is.na(df$rating) == TRUE , "Unrated", df$rating)
+rating.levels = c("G", "PG", "PG-13", "NC-17", "R", "Unrated")
+df$rating <- factor(df$rating, ordered=TRUE, levels=rating.levels)
+
 ### DROPPING COLUMNS
-df_usd <- subset(df_usd, select = - c(movie_url, budget, gross, runtime, URL_new, originalTitle, titleType, endYear, movie_id.y, Row.names, genres))
-
-view(df)
-                 
-######################################
-##### DATA EXPLORATION ###############
-######################################
-hist <- hist(df_usd$released, "weeks")
-
-###########################
-##### SPLITTING DATASET#### CHANGE LATER TO MOVIES UP 2017 (TEST IS 2018 and 2019 - 2020 and 2021 are going to be tests)
-###########################
-splitter = runif(nrow(df_usd))
-head(df_usd)
-## splitting dataset
-train <- filter(df_usd, splitter < 0.7)
-test <- filter(df_usd, splitter >= 0.7)
-view(train)
-view(test)
-
-######################################
-##### MODEL ##########################
-######################################
+df_usd <- subset(df_usd, select = - c(movie_url, budget, gross, runtime, originalTitle, titleType, endYear, movie_id.y, Row.names, genres))
 
 ### FACTORIZATION
 df_usd$director <- factor(df_usd$director)
@@ -145,10 +128,35 @@ df_usd$star <- factor(df_usd$star)
 df_usd$country <- factor(df_usd$country)
 df_usd$company <- factor(df_usd$company)
 
+### SOME OTHER ADJUSTEMENTS
+df_usd <- subset(df_usd, select = - c(...1))
+df_usd <- subset(df_usd, select = - c(isAdult))
+
+
+skim(df_usd)
+
+######################################
+##### DATA EXPLORATION ###############
+######################################
+hist <- hist(df_usd$released, "weeks")
+
+###########################
+##### SPLITTING DATASET#### CHANGE LATER TO MOVIES UP 2017 (TEST IS 2018 and 2019 - 2020 and 2021 are going to be tests)
+###########################
+head(df_usd)
+## splitting dataset
+train <- filter(df_usd, released <= "2016-12-31")
+test <- filter(df_usd, released >= "2017-01-01")
+view(train)
+view(test)
+
+######################################
+##### MODEL ##########################
+######################################
 
 ### LINEAR REGRESSION
 lm.1 <- lm(
-  train$`gross value` ~ `budget value` + year_widely_released + runtimeMinutes,
+  train$`gross value` ~ . -movie_id - `budget currency` - `gross currency` - primaryTitle - startYear,
   data = train)
 summary(lm.1)
 
@@ -166,7 +174,7 @@ OSR2_2
 ### CART MODEL
 library(rpart.plot)
 cv.trees <- train(y = train$`gross value`,
-                  x = subset(train, select=-c(`gross value`, id, director, writer, star, primaryTitle, company -Row.names)),
+                  x = subset(train, select=c(`gross value`, movie_id, primaryTitle, `budget currency`, `gross currency`, startYear)),
                   method = "rpart", 
                   trControl = trainControl(method = "cv", number = 10), 
                   tuneGrid = data.frame(.cp = seq(.00001,.0003,.000001)))
@@ -174,9 +182,12 @@ cv.trees$bestTune
 cv.results = cv.trees$results
 cv.results
 
-cart.model <- rpart(train$`gross value` ~. - id - director -writer -star - primaryTitle -company, data = train, control = rpart.control(cp = 0.000054))
+cart.model <- rpart(train$`gross value` ~. - movie_id - primaryTitle, data = train, control = rpart.control(cp = 0.000054))
 prp(cart.model, digits = 2, type = 2)
 
+r2_osr2_glmnet(cart.model, train, test, train$`gross value`, test$`gross value`, )
+
+#### FUNCTION FOR R2
 
 #### FUNCTION FOR R2 - NOT WORKING
 r2_osr2 <- function(tree, trainData, testData, yvar) {
@@ -207,3 +218,4 @@ r2_osr2 <- function(tree, trainData, testData, yvar) {
   print(paste0("OSR2=",OSR2))
 }
 
+r2_osr2(cart.model, train, test, `gross value`)
